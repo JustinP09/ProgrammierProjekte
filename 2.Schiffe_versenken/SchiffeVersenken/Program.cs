ea3bf045ba8632baaa5da87206d9004425d3fb95
JamainJamal <justinpichlmayer@gmail.com>
2025-03-23 22:21:56 +0100

    Schiffe_versenken

---
+++ 2.Schiffe_versenken/SchiffeVersenken/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.ComponentModel;
+
+namespace SchiffeVersenken
+{
+    internal class Program
+    {
+        static int AbfrageSpielfeldGroesse()    //hier wird nach der Spielfeldgroesse abgefragt 
+        {
+            bool fehlerEingabe;
+            int groesse;
+            do
+            {
+                fehlerEingabe = true;
+                Console.Write("Bitte geben Sie die gewünschte Spielfeld Groesse ein (5, 6, 7 Zeilen und Spalten): ");
+                groesse = Convert.ToInt32(Console.ReadLine());
+                if (groesse != 5 && groesse != 6 && groesse != 7)       //Wenn fehlerhafte Eingabe eingegeben wird, dann wiederhole
+                {
+                    Console.WriteLine("Ungueltige Eingabe, Bitte die Zahlen 5, 6 oder 7 eingeben!");
+                    fehlerEingabe = false;
+                }
+            } while (!fehlerEingabe);
+            return groesse;
+        }
+        static void PositioniereSchiff(int[,] spielfeld)
+        {
+            Random rand = new Random();
+            int ausrichtung = rand.Next(2);      //0 fuer wagrecht 1 fuer senkrecht
+            if (ausrichtung == 0)    //Damit mann nicht über den Spielfeldrand raus geht
+            {
+                int zeilenPosition = rand.Next(spielfeld.GetLength(0));
+                int spaltnePosition = rand.Next(spielfeld.GetLength(1) - 2);
+
+                for (int i = 0; i <= 2; i++)
+                {
+                    spielfeld[zeilenPosition, spaltnePosition + i] = 1;
+                }
+            }
+            else
+            {
+                int zeilenPosition = rand.Next(spielfeld.GetLength(0) - 2);
+                int spaltnePosition = rand.Next(spielfeld.GetLength(1));
+
+                for (int i = 0; i <= 2; i++)
+                {
+                    spielfeld[zeilenPosition + i, spaltnePosition] = 1;
+                }
+            }
+        }
+        static void ZeichnenSpielfeld(int[,] spielfeld)
+        {
+            Console.Write(" ");
+            for (int k = 1; k < spielfeld.GetLength(1) + 1; k++)   //erst Obereste Zeile Ausgeben (Spaltenbennenung)
+            {
+                Console.Write(" " + k);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < spielfeld.GetLength(0); i++)    //Jetzt Spielfeld Ausgeben 
+            {
+                Console.Write(i + 1);
+                for (int j = 0; j < spielfeld.GetLength(1); j++)
+                {
+                    switch (spielfeld[i, j])
+                    {
+                        case 0:
+                            Console.Write(" ~");
+                            break;
+                        case 1:
+                            Console.Write(" ~");
+                            break;
+                        case 2:
+                            Console.Write(" X");
+                            break;
+                        case 3:
+                            Console.Write(" O");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+        static int LeseGanzzahl(int groesse)    //Für die Eingabe des Schusses 
+        {
+            int zahl;
+            bool ganzzahl;
+            do
+            {
+                string eing = Console.ReadLine();
+                ganzzahl = int.TryParse(eing, out zahl);
+                if (ganzzahl == false)
+                {
+                    Console.Write("Die Zahl ist KEINE Ganzzahl, erneute Eingabe: ");
+
+                }
+                if (zahl > groesse || zahl <= 0)
+                {
+                    Console.Write("Die Zahl muss im Spielfeldbereich liegen, erneute Eingabe: ");
+                    ganzzahl = false;
+                }
+            } while (!ganzzahl);
+            return zahl - 1;    //-1, weil bsp. Array[0,0] der Eingabe 1,1 entspricht
+
+        }
+        static void Main(string[] args)
+        {
+            int groesse = AbfrageSpielfeldGroesse();
+            int[,] spielfeld = new int[groesse, groesse];
+            int zuege = 0, treffer = 0;
+            bool spielabbruch = true;
+            PositioniereSchiff(spielfeld);
+
+            do                  //Hauptgame Loop
+            {               
+                Console.Clear();
+                ZeichnenSpielfeld(spielfeld);
+
+                Console.Write("In welche Zeile moechtest Sie schiessen?: ");
+                int schussZeile = LeseGanzzahl(groesse);
+                Console.Write("In welche Spalte moechtest Sie schiessen?: ");
+                int schussSpalte = LeseGanzzahl(groesse);
+
+                switch (spielfeld[schussZeile, schussSpalte])
+                {
+                    case 0:
+                        spielfeld[schussZeile, schussSpalte] = 3;   //3 für Fehlschuss
+                        Console.WriteLine("Daneben!");
+                        break;
+                    case 1:
+                        spielfeld[schussZeile, schussSpalte] = 2;   //2 für getroffenes Schiff
+                        Console.WriteLine("Getroffen!");
+                        treffer++;
+                        break;
+                }
+                Console.ReadLine();
+                zuege++;
+                if (treffer == 3)
+                {
+                    spielabbruch = false;
+                }
+            } while (spielabbruch);
+            Console.Clear();
+            ZeichnenSpielfeld(spielfeld);
+            Console.WriteLine($"Sie haben gewonnen und dafür {zuege} Züge benötigt.\n");
+            Console.WriteLine("Taste zum Beenden drücken");
+        }
+    }
+}
---
+++ 3.Schafkopf_Notizen/SchafkopfSpiel/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchafkopfSpiel
+{
+    internal class Program
+    {
+
+        static void Spiel(int[,] spiel, string[] spieler)
+        {
+            for (int i = 0; i < spiel.GetLength(0); i++)
+            {
+                for (int j = 0; j < spiel.GetLength(1); j++)
+                {
+                    Console.Write($"Punkte von {spieler[j]} in Runde {i + 1}: ");
+                    spiel[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                if (!IstRichtig(spiel, i))
+                {
+                    Console.WriteLine("Stopp Falsche Eingabe! Nochmal eingeben");
+                    i--;
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool IstRichtig(int[,] a, int n)
+        {
+            int[] b = new int[a.GetLength(1)];
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = a[n, i];
+            }
+            int c = b[0];
+            for (int i = 1; i < b.Length; i++)
+            {
+                c += b[i];
+            }
+            if (c == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Striche(int anzSpieler)
+        {
+            for(int i = 0;i< anzSpieler*20; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+        static void Ausgabe(int[,] spiel, string[] spieler, int anzSpieler)
+        {
+            bool abbruch = false;
+            while (!abbruch)
+            {
+                Striche(anzSpieler);
+                Console.Write("         ");
+                foreach(string s in spieler)
+                {
+                    Console.Write($"{s,15}");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < spiel.GetLength(0); i++)
+                {
+                    string ausg = $"Spiel {i + 1}: ";
+                    for(int j = 0; j < spiel.GetLength(1); j++)
+                    {
+                        ausg += $"{spiel[i, j],15}";
+                    }
+                    Console.WriteLine(ausg);
+                }
+                int[] summe = new int[spiel.GetLength(1)];
+                Console.Write("Gesamt: ");
+                for (int i = 0;i < spiel.GetLength(1); i++)
+                {
+                    for(int j = 0;j < spiel.GetLength(0); j++)
+                    {
+                        summe[i] += spiel[j, i];
+                    }
+                    Console.Write($"{summe[i],16}");
+                }
+                Console.WriteLine();
+                Striche(anzSpieler);
+                Console.ReadLine();
+                abbruch = true;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Wie viele Spieler?: ");
+            int anzSpieler = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Wie viele Spiele willst du spielen?: ");
+            int anzSpiele = Convert.ToInt32(Console.ReadLine());
+
+            string[] spieler = new string[anzSpieler];
+            int[,] spiel = new int[anzSpiele, anzSpieler];
+
+            for (int i = 0; i < spieler.Length; i++)
+            {
+                Console.Write($"Name von Spieler {i + 1}: ");
+                spieler[i] = Console.ReadLine();
+            }
+            Console.Clear();
+            Spiel(spiel, spieler);
+            Ausgabe(spiel, spieler, anzSpieler);
+
+        }
+    }
+}
